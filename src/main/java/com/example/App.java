37488271ebfc1e464bc36e52dbacb4d2c54a7a49
BOGUDA JAYAVARDHAN <152011279+jayavardhan-10@users.noreply.github.com>
2025-02-24 22:09:20 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -10,8 +10,6 @@ public class App
 
     public static void main( String[] args )
     {
-
-
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! 2 you are great" );
 print(you r great)
